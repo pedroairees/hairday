0074942ba09319a34dd0c873164232e08f5bd5e6
pedroairees <pedrocoelho2209@gmail.com>
2026-01-29 18:11:23 -0300

    Sending a selected time to the API

--- src/modules/form/form.js
+++ src/modules/form/form.js
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 
 const form = document.querySelector("form");
 const inputDate = document.getElementById("date");
+const clienteName = document.getElementById("client");
 
 const dateToday = dayjs(new Date()).format("YYYY-MM-DD");
 
@@ -9,5 +10,24 @@ inputDate.value = dateToday;
 inputDate.min = dateToday;
 
 form.onsubmit = (event) => {
-    event.preventDefault();
-}
+  event.preventDefault();
+
+  try {
+    const name = clienteName.value.trim();
+    if (!name) {
+      return alert("Informe seu nome por favor!");
+    }
+    const hourSelected = document.querySelector(".hour-selected");
+    if (!hourSelected) {
+      return alert("Selecione um horário");
+    }
+
+    const [hour] = hourSelected.innerText.split(":");
+    console.log(hour);
+    const when = dayjs(dateToday.value).add(hour, "hour");
+    console.log(when);
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento!");
+    console.log(error);
+  }
+};
