c2baeb86c5d0121b3d50cafe4264dff0bcad3769
pedroairees <pedrocoelho2209@gmail.com>
2026-02-09 21:57:13 -0300

    searching for today's appointments

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,8 +1,10 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
 import { hoursLoad } from "../form/hours-load.js"
 
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
     const date = selectedDate.value;
+    const dailySchedules = await scheduleFetchByDay({ date });
     hoursLoad({ date });
 }
