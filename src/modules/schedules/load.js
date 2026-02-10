7d1c1e9cb05c780a46c5ebafdc9898bc6c8f6de6
pedroairees <pedrocoelho2209@gmail.com>
2026-02-10 08:46:08 -0300

    Rendering today's schedules

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,10 +1,12 @@
 import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
 import { hoursLoad } from "../form/hours-load.js"
+import { scheduleShow } from "./show.js"
 
 const selectedDate = document.getElementById("date");
 
 export async function schedulesDay() {
     const date = selectedDate.value;
     const dailySchedules = await scheduleFetchByDay({ date });
+    scheduleShow({ dailySchedules });
     hoursLoad({ date });
 }
